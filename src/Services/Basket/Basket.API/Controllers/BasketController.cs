925c3baaa8a40751d7d9376060bafaec76069788
Zethzer <bernard.y24@gmail.com>
2021-04-18 15:14:34 +0200

    Add basket controller

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{username}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var basket = await _repository.GetBasket(username);
+            
+            return Ok(basket ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{username}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            await _repository.DeleteBasket(username);
+            return Ok();
+        }
+    }
+}
