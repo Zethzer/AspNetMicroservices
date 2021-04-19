e8ad8adacfa711acc0c4abfd5574be07b77ca625
Zethzer <bernard.y24@gmail.com>
2021-04-19 23:49:41 +0200

    Remove unused usings

--- src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -2,9 +2,6 @@
 using Discount.API.Entities;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Discount.API.Repositories
