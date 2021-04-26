b9994f22432c128fafcea5719fd80e50d0d8fe29
Zethzer <bernard.y24@gmail.com>
2021-04-26 18:55:53 +0200

    Add behaviour + finish ordering.application

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.Application;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,8 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices();
+            //services.AddInfrastructureServices();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
