47f1541a622b01dbc538f816c875a922e87dbf3c
Zethzer <bernard.y24@gmail.com>
2021-05-01 19:46:52 +0200

    Prepare logiq for services and api calls

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,16 @@
+﻿using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
