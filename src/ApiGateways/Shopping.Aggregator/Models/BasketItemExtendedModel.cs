b27ef6837e6be7339f69fd1ae0c1f26d7729a6d1
Zethzer <bernard.y24@gmail.com>
2021-05-01 19:20:14 +0200

    Add models

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketItemExtendedModel
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+
+        // Product related additional fields
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
