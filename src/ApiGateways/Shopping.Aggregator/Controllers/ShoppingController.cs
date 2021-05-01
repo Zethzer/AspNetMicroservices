34759f0f48e38d59316fd14646b1daf1d17c19d0
Zethzer <bernard.y24@gmail.com>
2021-05-02 00:15:03 +0200

    Implement shoppingController + test in postman

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService,
+            IOrderService orderService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        [HttpGet("{username}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string username)
+        {
+            var basket = await _basketService.GetBasket(username);
+            foreach(var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await _orderService.GetOrdersByUsername(username);
+
+            return Ok(new ShoppingModel
+            {
+                Username = username,
+                BasketWithProducts = basket,
+                Orders = orders
+            });
+        }
+    }
+}
